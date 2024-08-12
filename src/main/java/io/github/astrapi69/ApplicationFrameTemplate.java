a8d59e9fcfac089152b3b6effe995e9c50ea5e23
Asterios Raptis <asteri.raptis@gmail.com>
2024-08-12 17:34:40 +0200

    formatted with spotless

--- src/main/java/io/github/astrapi69/ApplicationFrameTemplate.java
+++ src/main/java/io/github/astrapi69/ApplicationFrameTemplate.java
@@ -1,24 +1,26 @@
 package io.github.astrapi69;
 
-import lombok.extern.java.Log;
-
-import javax.swing.JFrame;
 import java.util.logging.Level;
 
+import javax.swing.JFrame;
+
+import lombok.extern.java.Log;
+
 @Log
-public class ApplicationFrameTemplate {
+public class ApplicationFrameTemplate
+{
 
-    public static void main(final String[] args)
-    {
-        log.setLevel(Level.FINE);
-        final JFrame frame = new JFrame("ApplicationFrameTemplate");
+	public static void main(final String[] args)
+	{
+		log.setLevel(Level.FINE);
+		final JFrame frame = new JFrame("ApplicationFrameTemplate");
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        frame.setSize(500, 400);
+		frame.setSize(500, 400);
 
-        frame.setLayout(null);
+		frame.setLayout(null);
 
-        frame.setVisible(true);
-    }
+		frame.setVisible(true);
+	}
 }
