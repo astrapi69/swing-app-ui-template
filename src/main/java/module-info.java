ec4cea99c5cb3e2ce99ba7bcc5933b76e0e9fc7d
Asterios Raptis <asteri.raptis@gmail.com>
2024-10-05 14:48:54 +0200

    added module-info.java file for modularization

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module swing.app.ui.template.main
+{
+	requires java.desktop;
+	requires java.logging;
+	requires io.github.astrapisixtynine.resourcebundle.core;
+	requires io.github.astrapisixtynine.swing.base.components;
+	requires jul.to.slf4j;
+	requires org.pf4j;
+	requires awt.extensions.main;
+	requires io.github.astrapisixtynine.menu.action;
+	requires model.data;
+	requires static lombok;
+}
