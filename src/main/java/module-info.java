9f2261292fe761464bdf1d62c0ec3e99b77cbcd8
Asterios Raptis <asteri.raptis@gmail.com>
2024-10-08 18:24:03 +0200

    update of all dependencies with gradle-plugin version-catalog-update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -24,10 +24,10 @@ module swing.app.ui.template.main
 	requires java.logging;
 	requires io.github.astrapisixtynine.resourcebundle.core;
 	requires io.github.astrapisixtynine.swing.base.components;
+	requires io.github.astrapisixtynine.awt.extensions;
+	requires io.github.astrapisixtynine.menu.action;
 	requires jul.to.slf4j;
 	requires org.pf4j;
-	requires awt.extensions.main;
-	requires io.github.astrapisixtynine.menu.action;
 	requires model.data;
 	requires static lombok;
 }
